27ac7dcc8dbb8f4cd3b6e3404ca0cbc42cceb85a
Gabriellbragaa <gabrielbragamartins.gb@gmail.com>
2025-06-04 10:58:24 -0300

    java Enum

--- DiasSemanas/TestEnum.java
+++ DiasSemanas/TestEnum.java
@@ -2,6 +2,10 @@ public class TestEnum {
 
     public static void main(String[] args) {
         usandoEnum();
+
+        System.out.println();
+
+      //  usandoConstantes();
     }
    
     public enum DiasSemanas {
@@ -14,12 +18,38 @@ public class TestEnum {
     DOMINGO;
     }
 
+    public class DiasSemanasConstantes{
+        public static final int SEGUNDA = 1;
+        public static final int TERCA = 2;
+        public static final int QUARTA = 3;
+        public static final int QUINTA = 4;
+        public static final int SEXTA = 5;
+        public static final int SABADO = 6;
+
+    }
+
+   /*  
+   
+    private static void usandoConstantes(){
+
+        int segunda = DiasSemanasConstantes.SEGUNDA;
+        int terca = DiasSemanasConstantes.TERCA;
+        int quarta = DiasSemanasConstantes.QUARTA;
+        int quinta = DiasSemanasConstantes.QUINTA;
+
+        System.out.println("Teste Unitario utilizando enum no Java");
+        imprimirDiaSemana(segunda);
+        imprimirDiaSemana(terca);
+
+    }
+
+    */
 
     private static void usandoEnum() {
         DiasSemanas segunda = DiasSemanas.SEGUNDA;
         DiasSemanas terca = DiasSemanas.TERCA;
 
-        System.out.println("Teste utilizando enum no Java");
+        System.out.println("Teste Unitario utilizando enum no Java");
         imprimirDiaSemana(segunda);
         imprimirDiaSemana(terca);
     }
